8cb4247882e63145ecd168b6d9bb956f73d2db29
Vincent Ho <vincenthohoho123@gmail.com>
2024-08-13 20:15:59 -0700

    mobile friendly

--- src/Resume.js
+++ src/Resume.js
@@ -1,28 +1,28 @@
-// Resume.js
 import React from 'react';
-import resume from './Assets/VincentResume.pdf'; // Update the path to where you’ve uploaded your resume PDF
+import resume from './Assets/VincentResume.pdf';
 
 const Resume = () => {
   return (
     <div style={{ marginTop: '120px', padding: '20px', textAlign: 'center' }}>
       <div style={{ 
-        width: '950px', // Fixed width
-        height: '90vh', // Viewport height for a responsive design
-        margin: '0 auto', // Center the container
-        overflow: 'hidden' // Hide any overflow
+        width: '100%', 
+        maxWidth: '950px', 
+        height: '90vh', 
+        margin: '0 auto', 
+        overflow: 'hidden' 
       }}>
         <iframe
           src={resume}
-          style={{ width: '90%', height: '100%', border: 'none' }}
+          style={{ 
+            width: '100%', 
+            height: '100%', 
+            border: 'none' 
+          }}
           title="Resume"
         ></iframe>
       </div>
       
-      <a
-        href={resume}
-        download
-        style={{ textDecoration: 'none' }}
-      >
+      <a href={resume} download style={{ textDecoration: 'none' }}>
         <button type="button" className="btn btn-outline-light" style={{ marginTop: '40px' }}>
           Download Resume
         </button>
