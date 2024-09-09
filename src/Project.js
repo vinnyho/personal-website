30d4b47111549b910783ffd5bff8a53560d6ea42
Vincent Ho <vincenthohoho123@gmail.com>
2024-09-08 19:55:33 -0700

    fix

--- src/Project.js
+++ src/Project.js
@@ -5,7 +5,7 @@ import ProjectCard from './ProjectCard';
 const Project = () => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <ProjectCard />
+    
     </div>
   );
 };
